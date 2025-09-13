1636d0eff9c86d82d8cd2d2050a5a54fbcf99518
v_vilimanovic <vilimanovicvojin@gmail.com>
2025-09-13 16:20:47 +0200

    Add project files.

---
+++ WPFModelClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFModelClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
