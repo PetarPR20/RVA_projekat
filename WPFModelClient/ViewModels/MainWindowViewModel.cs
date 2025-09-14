e3c3aa9cf5416fe06b2d4d6b0c83334c50c02bdb
v_vilimanovic <vilimanovicvojin@gmail.com>
2025-09-14 19:41:46 +0200

    Uspesno kreirana veza izmedju glavnog prozora i njegovog ViewModela.

---
+++ WPFModelClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFModelClient.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string _testText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public string TestText
+        {
+            get
+            {
+                return _testText;
+            }
+            set
+            {
+                if (_testText != value)
+                {
+                    _testText = value;
+                    OnPropertyChanged(nameof(TestText));
+                }
+            }
+        }
+        public MainWindowViewModel()
+        {
+            TestText = "Hello World";
+        }
+    }
+}
